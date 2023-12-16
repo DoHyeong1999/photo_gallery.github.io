60940ece46cd6f11e7f338c08a8c09594be3a3d1
DoHyeong1999 <ehguddk88@gmail.com>
2023-12-16 23:43:20 +0900

    JS:Arrow-up

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', ()=> {
-  console.log(1 - window.scrollY / homeHeight);
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 // KAWAGUCHI 아래로 스크롤시 투명하게 처리
@@ -12,3 +11,13 @@ document.addEventListener('scroll', ()=> {
 //   console.log(1 - window.scrollY / kawaguchiHeight);
 //   kawaguchi.style.opacity = 1 - window.scrollY / kawaguchiHeight;
 // });
+
+// Arrow up버튼을 아래로 스크롤시 투명하게 처리
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', ()=> {
+  if(window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+});
